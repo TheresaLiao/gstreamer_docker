19ca6b148abb830f946ee115a23efc2d2ba2378a
TheresaLiao <33051006+TheresaLiao@users.noreply.github.com>
2024-04-09 13:31:55 +0800

    Create run_cnt_x86_gpu_12_0.sh

---
+++ 99.Final_version/run_cnt_x86_gpu_12_0.sh
@@ -0,0 +1,17 @@
+container_name=gst-cnt-gpu_12_0
+image_name=gst_python3_opencv:x86_gpu_12_0
+dockerfile_name=Dockerfile.x86_gpu_12_0
+
+docker stop $container_name
+docker rm $container_name
+
+docker build -t $image_name -f $dockerfile_name .
+
+xhost +local:docker
+docker run -ti -d \
+       -w /home \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -e DISPLAY=$DISPLAY \
+       -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,video \
+       --name $container_name \
+       $image_name bash
